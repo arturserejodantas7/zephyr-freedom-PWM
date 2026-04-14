d653dd021a3e0bee77ead61abe24fb434b1b1e52
ARTUR SEREJO DANTAS <arturserejodantas7@usp.br>
2026-04-14 10:24:10 -0300

    4.1 Teste Motor Único

--- src/main.c
+++ src/main.c
@@ -1,35 +1,49 @@
-#include <zephyr/kernel.h>             // Funções básicas do Zephyr (ex: k_msleep, k_thread, etc.)
-#include <zephyr/device.h>             // API para obter e utilizar dispositivos do sistema
-#include <zephyr/drivers/gpio.h>       // API para controle de pinos de entrada/saída (GPIO)
-#include <pwm_z42.h>                // Biblioteca personalizada com funções de controle do TPM (Timer/PWM Module)
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <pwm_z42.h>
 
-// Define o valor do registrador MOD do TPM para configurar o período do PWM
-#define TPM_MODULE 1000         // Define a frequência do PWM fpwm = (TPM_CLK / (TPM_MODULE * PS))
-// Valores de duty cycle correspondentes a diferentes larguras de pulso
-uint16_t duty_50  = TPM_MODULE/2;       // 50% de duty cycle (meio brilho)
+#define TPM_MODULE 1000
+
+// GPIO
+#define IN1_PIN 0
+#define IN2_PIN 1
+
+const struct device *gpio_dev = DEVICE_DT_GET(DT_NODELABEL(gpiob));
 
 int main(void)
 {
-    // Inicializa o módulo TPM2 com:
-    // - base do TPMx
-    // - fonte de clock PLL/FLL (TPM_CLK)
-    // - valor do registrador MOD
-    // - tipo de clock (TPM_CLK)
-    // - prescaler de 1 a 128 (PS)
-    // - modo de operação EDGE_PWM
-    pwm_tpm_Init(TPM2, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
+    uint16_t duty = 0;
 
-    // Inicializa o canal 0 do TPM2 para gerar sinal PWM na porta GPIOB_18
-    // - modo TPM_PWM_H (nível alto durante o pulso)
-    pwm_tpm_Ch_Init(TPM2, 0, TPM_PWM_H, GPIOB, 18);
+    // Inicializa PWM
+    pwm_tpm_Init(TPM0, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
 
-    // Define o valor do duty cycle: nesse caso, duty_100 (LED quase desligado)
-    pwm_tpm_CnV(TPM2, 0, duty_50);
+    // ENA → PTD0
+    pwm_tpm_Ch_Init(TPM0, 0, TPM_PWM_H, GPIOD, 0);
 
-    // Loop infinito
-    for (;;)
+    // Configura GPIO
+    gpio_pin_configure(gpio_dev, IN1_PIN, GPIO_OUTPUT);
+    gpio_pin_configure(gpio_dev, IN2_PIN, GPIO_OUTPUT);
+
+    // Motor sempre para frente
+    gpio_pin_set(gpio_dev, IN1_PIN, 1);
+    gpio_pin_set(gpio_dev, IN2_PIN, 0);
+
+    while (1)
     {
-        // O programa poderia alterar o duty cycle dinamicamente aqui se desejado
+        // Aumenta velocidade
+        for (duty = 0; duty < TPM_MODULE; duty += 50)
+        {
+            pwm_tpm_CnV(TPM0, 0, duty);
+          
+        }
+
+        // Diminui velocidade
+        for (duty = TPM_MODULE; duty > 0; duty -= 50)
+        {
+            pwm_tpm_CnV(TPM0, 0, duty);
+            
+        }
     }
 
     return 0;
